f6acc9b25d8bda6f0be72593face6f42a2ebb679
AvtarSinghChundawat <singavtar999@gmail.com>
2025-06-13 19:01:15 +0530

    Update button positioning in Todo component for better layout consistency

--- src/App.jsx
+++ src/App.jsx
@@ -226,7 +226,7 @@ function App() {
 
   return (
     <div className={`wrapper w-screen flex justify-center h-full ${isDark ? 'bg-[#343434] text-white' : 'bg-[#F7F7F7] text-black'}`}>
-      <div className="main flex flex-col items-center w-[80%] sm:w-[70%]">
+      <div className="main flex flex-col items-center relative w-[80%] sm:w-[70%]">
         <header className='flex w-screen justify-center items-center p-5 text-[2em] mt-5'>TODO LIST</header>
         <div className="search w-full flex gap-4">
           <div className={`rounded-[7px] input flex w-full gap-1 items-center border ${isDark ? 'border-white' : 'border-black'} pr-2 pl-2`}>
@@ -293,7 +293,7 @@ function App() {
               )}
             </button>
           </div>
-          <button onClick={handleShowModal} className={`cursor-pointer addTodo relative md:fixed md:bottom-[30px] md:right-[30px] bg-[#6C63FF] md:p-3 rounded-[50%] flex justify-center items-center hover:shadow-[0_0_10px_rgba(124,115,255,0.6)] hover:bg-[#7B73FF]`} style={{ minHeight: window.innerWidth < 768 ? `${buttonHeight}px` : 'auto' }}>
+          <button onClick={handleShowModal} className={`cursor-pointer addTodo relative md:absolute md:bottom-[30px] md:right-0 bg-[#6C63FF] md:p-3 rounded-[50%] flex justify-center items-center hover:shadow-[0_0_10px_rgba(124,115,255,0.6)] hover:bg-[#7B73FF]`} style={{ minHeight: window.innerWidth < 768 ? `${buttonHeight}px` : 'auto' }}>
             <svg width="24px" height="24px" viewBox="-1.4 -1.4 22.80 22.80" xmlns="http://www.w3.org/2000/svg" fill="white" stroke="white" stroke-width="1.42"><g id="SVGRepo_bgCarrier" stroke-whiteidth="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="" stroke-whiteidth="0.24"></g><g id="SVGRepo_iconCarrier"> <path fill="white" d="M10,-1.77635684e-15 C10.4232029,-1.69861573e-15 10.7662767,0.343073746 10.7662767,0.766276659 L10.766,9.233 L19.2337233,9.23372334 C19.6569263,9.23372334 20,9.57679709 20,10 C20,10.4232029 19.6569263,10.7662767 19.2337233,10.7662767 L10.766,10.766 L10.7662767,19.2337233 C10.7662767,19.6569263 10.4232029,20 10,20 C9.57679709,20 9.23372334,19.6569263 9.23372334,19.2337233 L9.233,10.766 L0.766276659,10.7662767 C0.343073746,10.7662767 0,10.4232029 0,10 C0,9.57679709 0.343073746,9.23372334 0.766276659,9.23372334 L9.233,9.233 L9.23372334,0.766276659 C9.23372334,0.343073746 9.57679709,-1.85409795e-15 10,-1.77635684e-15 Z"></path> </g></svg>
           </button>
         </div>
