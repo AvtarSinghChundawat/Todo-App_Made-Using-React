853cfc4f7b71cdf605231f9bc52177552ff79446
AvtarSinghChundawat <singavtar999@gmail.com>
2025-06-13 19:43:52 +0530

    Adjust width of Todo component layout for improved responsiveness

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -23,7 +23,7 @@ export const Todo = (props) => {
 
     return (
         <div className={`w-[100%] rounded-[0px] shrink-0 border-t ${props.isDark ? 'border-white' : 'border-black'} pt-[15px] p-2 flex justify-between items-center`}>
-            <div className="flex gap-3 items-center w-[80%]">
+            <div className="flex gap-3 items-center w-[76%]">
                 {/* Checkbox: controls completed status via parent */}
                 <label htmlFor={`checkbox-${props.id}`} className="relative flex items-center cursor-pointer group">
                     {/* Visually hidden native checkbox */}
